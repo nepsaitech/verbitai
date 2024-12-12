107fcfaeb898c8cfdefbaaee93153daa36831c5f
Nep Enad <enadnep@gmail.com>
2024-12-12 17:14:58 +0800

    Remove comment

--- page-mailer.php
+++ page-mailer.php
@@ -6,20 +6,4 @@ Template Post Type: page
 
 get_header();
 
-/* get_template_part( 'src/blocks/layouts/pages/mailer/site-head' ); */
-
 get_template_part( 'src/blocks/layouts/pages/mailer/banner' ); 
-
-/* 
-
-if ( have_rows( 'mailer_blocks' ) ) {
-    while ( have_rows( 'mailer_blocks' ) ) {
-        the_row();
-
-        $dir = 'src/blocks/layouts/pages/mailer/banner.php';
-        
-        include $dir . get_row_layout() . '.php';
-    }
-} */
-
-/* get_template_part( 'src/blocks/layouts/pages/mailer/footer' ); */
